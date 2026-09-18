d786db2ac2e38e719d0b6fc4962db5931845f40b
mrleninplaymrctalinplay-pixel <mrleninplaymrctalinplay@gmail.com>
2026-09-18 11:11:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-# Токен вашего бота
-API_TOKEN = "8996747968:AAGiV1p5kHoy-gQ2YDVknlmD2h3snSVe3sI"
+# Новый токен вашего бота
+API_TOKEN = "8996747968:AAGt-wgqjd2Ao8stQezE_-othXKG3SC3Z54"
 
-# Включаем логирование
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Приветственное сообщение при старте бота в личке
 @dp.message(Command("start"))
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Бот GreeLand успешно запущен и готов к работе.")
 
-# Обработка нажатия кнопок «Одобрить» / «Отклонить» под анктой в группе
 @dp.callback_query(lambda c: c.data in ["app_approve", "app_reject"])
 async def process_app_buttons(callback_query: types.CallbackQuery):
     message = callback_query.message
@@ -45,7 +41,6 @@ async def process_app_buttons(callback_query: types.CallbackQuery):
         await callback_query.answer("Анкета отклонена.")
 
 async def main():
-    # Запуск поллинга для aiogram v3
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
